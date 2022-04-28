1ac938ed0bb19fe3e432e777cf12f6b97436c450
Jonathan Potter <jonpot@umich.edu>
2022-04-28 12:41:59 -0400

    Add cluster detection to determine data location

--- examples/nltk-test.py
+++ examples/nltk-test.py
@@ -10,12 +10,31 @@ spark = SparkSession.builder \
           .appName("nltk-test") \
           .getOrCreate()
 
+# Take clustername and return input data file path.
+def setInputFile(cluster):
+    location = {
+        "greatlakes": "/nfs/turbo/arcts-data-hadoop-stage/data/complete-works-of-shakespeare.txt",
+        "thunderx": "/data/complete-works-of-shakespeare.txt"
+    }
+    return location.get(cluster, "/data/complete-works-of-shakespeare.txt")
+
 #partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 24
-partitions = 24
+partitions = 12
 nltk_data_location = "/nfs/turbo/arcts-data-hadoop-stage/data/nltk_data/"
 
 start = time.time()
-data_location = str(sys.argv[1]) if len(sys.argv) > 1 else '/data/complete-works-of-shakespeare.txt'
+
+if len(sys.argv) > 1:
+    data_location = str(sys.argv[1])
+else:
+    if "CLUSTER_NAME" in os.environ:
+        data_location = setInputFile(os.environ['CLUSTER_NAME'])
+    else:
+        print("Cannot identify HPC cluster name because CLUSTER_NAME env var \
+            is missing. Try passing the path to the input files as an argument. \
+            Will assume we are running on Cavium ThunderX.")
+        data_location = setInputFile("thunderx")
+
 data = spark.sparkContext.textFile(data_location, partitions)
 
 def word_tokenize(x):
@@ -36,5 +55,7 @@ pos_word = words.map(pos_tag)
 print(pos_word.take(20))
 end = time.time()
 
-print("Completed dataset tokenization and part of speech tagging of first 20 elements in {0} seconds."
+print("############################################################################")
+print("NLTK works. Completed dataset tokenization and part of speech tagging of first 20 elements in {0} seconds."
       .format(round(end - start, 2)))
+print("############################################################################")
